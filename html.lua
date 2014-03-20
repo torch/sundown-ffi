812223802c9c9f359b46478b7bbb1d5b154a88ac
Ronan Collobert <ronan@collobert.com>
2014-03-20 11:53:38 +0100

    fix mem leak in html renderer

--- html.lua
+++ html.lua
@@ -9,19 +9,15 @@ local function render(txt)
    local callbacks = ffi.new('struct sd_callbacks')
    local options = ffi.new('struct sd_html_renderopt')
    C.sd_html_renderer(callbacks, options, 0)
-
    local markdown = C.sd_markdown_new(0xfff, 16, callbacks, options)
 
-   local outbuf = ffi.new('struct sd_buf')
-   outbuf.data = nil
-   outbuf.size = 0
-   outbuf.asize = 0
-   outbuf.unit = 64
-
+   local outbuf = C.sd_bufnew(64)
    C.sd_markdown_render(outbuf, ffi.cast('const char*', txt), #txt, markdown)
    C.sd_markdown_free(markdown)
+   txt = ffi.string(outbuf.data, outbuf.size)
+   C.sd_bufrelease(outbuf)
 
-   return ffi.string(outbuf.data, outbuf.size)
+   return txt
 end
 
 return {render=render}
